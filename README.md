f8129ba306e753b6beca2b8765dabad62cc9d975
philip bankier <philipbankier@gmail.com>
2017-08-29 21:02:44 -0400

    Added the Tetris  kotlin maven port

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Tetris
+Tetris in Kotlin, designed for use with maven-tiles project.
+## Installation
+Either download this repository as a zip or clone it.
+
+## Usage
+
+## Contributing
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
--- src/kotlin/Tetris.kt
+++ src/kotlin/Tetris.kt
@@ -5,45 +5,149 @@ import java.awt.event.KeyEvent
 import java.awt.event.KeyListener
 import java.util.ArrayList
 import java.util.Collections
-
 import javax.swing.JFrame
 import javax.swing.JPanel
 
 class Tetris : JPanel() {
 
-    private val Tetraminos = arrayOf(
+    private val kotlinTetris = arrayOf(
             // I-Piece
-            arrayOf(arrayOf(Point(0, 1), Point(1, 1), Point(2, 1), Point(3, 1)), arrayOf(Point(1, 0), Point(1, 1), Point(1, 2), Point(1, 3)), arrayOf(Point(0, 1), Point(1, 1), Point(2, 1), Point(3, 1)), arrayOf(Point(1, 0), Point(1, 1), Point(1, 2), Point(1, 3))),
-
+            arrayOf(arrayOf(
+                    Point(0, 1),
+                    Point(1, 1),
+                    Point(2, 1),
+                    Point(3, 1)),
+                    arrayOf(Point(1, 0),
+                            Point(1, 1),
+                            Point(1, 2),
+                            Point(1, 3)),
+                    arrayOf(Point(0, 1),
+                            Point(1, 1),
+                            Point(2, 1),
+                            Point(3, 1)),
+                    arrayOf(Point(1, 0),
+                            Point(1, 1),
+                            Point(1, 2),
+                            Point(1, 3))),
             // J-Piece
-            arrayOf(arrayOf(Point(0, 1), Point(1, 1), Point(2, 1), Point(2, 0)), arrayOf(Point(1, 0), Point(1, 1), Point(1, 2), Point(2, 2)), arrayOf(Point(0, 1), Point(1, 1), Point(2, 1), Point(0, 2)), arrayOf(Point(1, 0), Point(1, 1), Point(1, 2), Point(0, 0))),
-
+            arrayOf(arrayOf(
+                    Point(0, 1),
+                    Point(1, 1),
+                    Point(2, 1),
+                    Point(2, 0)),
+                    arrayOf(Point(1, 0),
+                            Point(1, 1),
+                            Point(1, 2),
+                            Point(2, 2)),
+                    arrayOf(Point(0, 1),
+                            Point(1, 1),
+                            Point(2, 1),
+                            Point(0, 2)),
+                    arrayOf(Point(1, 0),
+                            Point(1, 1),
+                            Point(1, 2),
+                            Point(0, 0))),
             // L-Piece
-            arrayOf(arrayOf(Point(0, 1), Point(1, 1), Point(2, 1), Point(2, 2)), arrayOf(Point(1, 0), Point(1, 1), Point(1, 2), Point(0, 2)), arrayOf(Point(0, 1), Point(1, 1), Point(2, 1), Point(0, 0)), arrayOf(Point(1, 0), Point(1, 1), Point(1, 2), Point(2, 0))),
-
+            arrayOf(arrayOf(
+                    Point(0, 1),
+                    Point(1, 1),
+                    Point(2, 1),
+                    Point(2, 2)),
+                    arrayOf(Point(1, 0),
+                            Point(1, 1),
+                            Point(1, 2),
+                            Point(0, 2)),
+                    arrayOf(Point(0, 1),
+                            Point(1, 1),
+                            Point(2, 1),
+                            Point(0, 0)),
+                    arrayOf(Point(1, 0),
+                            Point(1, 1),
+                            Point(1, 2),
+                            Point(2, 0))),
             // O-Piece
-            arrayOf(arrayOf(Point(0, 0), Point(0, 1), Point(1, 0), Point(1, 1)), arrayOf(Point(0, 0), Point(0, 1), Point(1, 0), Point(1, 1)), arrayOf(Point(0, 0), Point(0, 1), Point(1, 0), Point(1, 1)), arrayOf(Point(0, 0), Point(0, 1), Point(1, 0), Point(1, 1))),
-
+            arrayOf(arrayOf(
+                    Point(0, 0),
+                    Point(0, 1),
+                    Point(1, 0),
+                    Point(1, 1)),
+                    arrayOf(Point(0, 0),
+                            Point(0, 1),
+                            Point(1, 0),
+                            Point(1, 1)),
+                    arrayOf(Point(0, 0),
+                            Point(0, 1),
+                            Point(1, 0),
+                            Point(1, 1)),
+                    arrayOf(Point(0, 0),
+                            Point(0, 1),
+                            Point(1, 0),
+                            Point(1, 1))),
             // S-Piece
-            arrayOf(arrayOf(Point(1, 0), Point(2, 0), Point(0, 1), Point(1, 1)), arrayOf(Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 2)), arrayOf(Point(1, 0), Point(2, 0), Point(0, 1), Point(1, 1)), arrayOf(Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 2))),
-
+            arrayOf(arrayOf(
+                    Point(1, 0),
+                    Point(2, 0),
+                    Point(0, 1),
+                    Point(1, 1)),
+                    arrayOf(Point(0, 0),
+                            Point(0, 1),
+                            Point(1, 1),
+                            Point(1, 2)),
+                    arrayOf(Point(1, 0),
+                            Point(2, 0),
+                            Point(0, 1),
+                            Point(1, 1)),
+                    arrayOf(Point(0, 0),
+                            Point(0, 1),
+                            Point(1, 1),
+                            Point(1, 2))),
             // T-Piece
-            arrayOf(arrayOf(Point(1, 0), Point(0, 1), Point(1, 1), Point(2, 1)), arrayOf(Point(1, 0), Point(0, 1), Point(1, 1), Point(1, 2)), arrayOf(Point(0, 1), Point(1, 1), Point(2, 1), Point(1, 2)), arrayOf(Point(1, 0), Point(1, 1), Point(2, 1), Point(1, 2))),
-
+            arrayOf(arrayOf(
+                    Point(1, 0),
+                    Point(0, 1),
+                    Point(1, 1),
+                    Point(2, 1)),
+                    arrayOf(Point(1, 0),
+                            Point(0, 1),
+                            Point(1, 1),
+                            Point(1, 2)),
+                    arrayOf(Point(0, 1),
+                            Point(1, 1),
+                            Point(2, 1),
+                            Point(1, 2)),
+                    arrayOf(Point(1, 0),
+                            Point(1, 1),
+                            Point(2, 1),
+                            Point(1, 2))),
             // Z-Piece
-            arrayOf(arrayOf(Point(0, 0), Point(1, 0), Point(1, 1), Point(2, 1)), arrayOf(Point(1, 0), Point(0, 1), Point(1, 1), Point(0, 2)), arrayOf(Point(0, 0), Point(1, 0), Point(1, 1), Point(2, 1)), arrayOf(Point(1, 0), Point(0, 1), Point(1, 1), Point(0, 2))))
+            arrayOf(arrayOf(
+                    Point(0, 0),
+                    Point(1, 0),
+                    Point(1, 1),
+                    Point(2, 1)),
+                    arrayOf(Point(1, 0),
+                            Point(0, 1),
+                            Point(1, 1),
+                            Point(0, 2)),
+                    arrayOf(Point(0, 0),
+                            Point(1, 0),
+                            Point(1, 1),
+                            Point(2, 1)),
+                    arrayOf(Point(1, 0),
+                            Point(0, 1),
+                            Point(1, 1),
+                            Point(0, 2))))
 
-    private val tetraminoColors = arrayOf(Color.cyan, Color.blue, Color.orange, Color.yellow, Color.green, Color.pink, Color.red)
+    private val availColors = arrayOf(Color.cyan, Color.blue, Color.orange, Color.yellow, Color.green, Color.pink, Color.red)
 
     private var pieceOrigin: Point? = null
     private var currentPiece: Int = 0
     private var rotation: Int = 0
     private val nextPieces = ArrayList<Int>()
-
     private var score: Long = 0
     private var well: Array<Array<Color>>? = null
 
-    // Creates a border around the well and initializes the dropping piece
+    // prints border and initializes the dropping piece
     private fun init() {
 
         well = Array<Array<Color>>(12 , { Array<Color>(24) { Color.BLACK } })
@@ -59,7 +163,7 @@ class Tetris : JPanel() {
         newPiece()
     }
 
-    // Put a new, random piece into the dropping position
+    // Put random piece into the dropping position
     fun newPiece() {
         pieceOrigin = Point(5, 2)
         rotation = 0
@@ -71,9 +175,9 @@ class Tetris : JPanel() {
         nextPieces.removeAt(0)
     }
 
-    // Collision test for the dropping piece
+    // Test for the dropping piece
     private fun collidesAt(x: Int, y: Int, rotation: Int): Boolean {
-        for (p in Tetraminos[currentPiece][rotation]) {
+        for (p in kotlinTetris[currentPiece][rotation]) {
             if (well!![p.x + x][p.y + y] !== Color.BLACK) {
                 return true
             }
@@ -101,7 +205,7 @@ class Tetris : JPanel() {
         repaint()
     }
 
-    // Drops the piece one line or fixes it to the well if it can't drop
+    // Drops the piece one line or stops it
     fun dropDown() {
         if (!collidesAt(pieceOrigin!!.x, pieceOrigin!!.y + 1, rotation)) {
             pieceOrigin!!.y += 1
@@ -111,11 +215,11 @@ class Tetris : JPanel() {
         repaint()
     }
 
-    // Make the dropping piece part of the well, so it is available for
+    // Make the dropping piece part of the ground, so it is available for
     // collision detection.
     fun fixToWell() {
-        for (p in Tetraminos[currentPiece][rotation]) {
-            well!![pieceOrigin!!.x + p.x][pieceOrigin!!.y + p.y] = tetraminoColors[currentPiece]
+        for (p in kotlinTetris[currentPiece][rotation]) {
+            well!![pieceOrigin!!.x + p.x][pieceOrigin!!.y + p.y] = availColors[currentPiece]
         }
         clearRows()
         newPiece()
@@ -161,9 +265,9 @@ class Tetris : JPanel() {
     }
 
     // Draw the falling piece
-    private fun drawPiece(g: Graphics) {
-        g.color = tetraminoColors[currentPiece]
-        for (p in Tetraminos[currentPiece][rotation]) {
+    private fun printPiece(g: Graphics) {
+        g.color = availColors[currentPiece]
+        for (p in kotlinTetris[currentPiece][rotation]) {
             g.fillRect((p.x + pieceOrigin!!.x) * 26,
                     (p.y + pieceOrigin!!.y) * 26,
                     25, 25)
@@ -171,7 +275,6 @@ class Tetris : JPanel() {
     }
 
     public override fun paintComponent(g: Graphics) {
-        // Paint the well
         g.fillRect(0, 0, 26 * 12, 26 * 23)
         for (i in 0..11) {
             for (j in 0..22) {
@@ -185,7 +288,7 @@ class Tetris : JPanel() {
         g.drawString("" + score, 19 * 12, 25)
 
         // Draw the currently falling piece
-        drawPiece(g)
+        printPiece(g)
     }
 
     companion object {
@@ -218,7 +321,6 @@ class Tetris : JPanel() {
                         }
                     }
                 }
-
                 override fun keyReleased(e: KeyEvent) {}
             })
 
@@ -231,7 +333,6 @@ class Tetris : JPanel() {
                             game.dropDown()
                         } catch (e: InterruptedException) {
                         }
-
                     }
                 }
             }.start()
